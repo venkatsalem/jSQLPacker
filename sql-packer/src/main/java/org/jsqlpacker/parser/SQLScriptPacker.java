f3b589addca7dc24cf638281e1257c1f86b75312
venkatsalem <venkatsalem@gmail.com>
2014-03-10 14:23:34 -0400

    2nd commit

---
+++ sql-packer/src/main/java/org/jsqlpacker/parser/SQLScriptPacker.java
@@ -0,0 +1,14 @@
+package org.jsqlpacker.parser;
+
+/**
+ * Packages given SQL Scripts as a tool to run by DBAs
+ * 
+ * @author venkatsalem@gmail.com
+ */
+public class SQLScriptPacker {
+
+	public static void main(String[] args) {
+
+	}
+
+}
