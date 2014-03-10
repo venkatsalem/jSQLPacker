999d3208dfddafc5e489ab38ee5ed4bfccc986cb
venkatsalem <venkatsalem@gmail.com>
2014-03-10 14:20:35 -0400

    Added 1st file

---
+++ sql-packer/src/main/java/org/jsqlpacker/parser/SQLScriptParser.java
@@ -0,0 +1,14 @@
+package org.jsqlpacker.parser;
+
+/**
+ * Class parses the SQL Script and converts to XML
+ * 
+ * @author venkatsalem@gmail.com
+ */
+public class SQLScriptParser {
+
+	public static void main(String[] args) {
+
+	}
+
+}
